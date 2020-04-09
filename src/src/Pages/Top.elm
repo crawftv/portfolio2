f406635e645b946ad59fbc4b8511ef3d88ccb61e
Crawford Collins <crawford.collins@gmail.com>
2020-04-09 17:23:09 -0500

    started working on elm-spa

---
+++ src/src/Pages/Top.elm
@@ -0,0 +1,31 @@
+module Pages.Top exposing (Flags, Model, Msg, page)
+
+import Html
+import Page exposing (Document, Page)
+import Element
+
+
+type alias Flags =
+    ()
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+page : Page Flags Model Msg
+page =
+    Page.static
+        { view = view
+        }
+
+
+view : Document Msg
+view =
+    { title = "Home Page"
+    , body = [ Element.text "Top" ]
+    }
