bc5c1940b7af7decb41dcfad08999e55f4a9dc64
Crawford Collins <crawford.collins@gmail.com>
2020-04-10 10:52:31 -0500

    render elm through jinja

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import falcon
+from falcon_jinja2 import FalconTemplate
 
 api = application = falcon.API()
-
+falcon_template = FalconTemplate(path=".")
 class Home(object):
+    @falcon_template.render("index.html")
     def on_get(self,req,resp):
-        doc = "Home Page"
         resp.status =falcon.HTTP_200
-        resp.body=(doc)
 
 
 home = Home()
